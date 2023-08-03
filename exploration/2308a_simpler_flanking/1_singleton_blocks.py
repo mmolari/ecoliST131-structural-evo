b2533248a644439afed6a45c930885f4ece19169
Marco Molari <mmolari91@gmail.com>
2023-08-03 15:54:02 +0200

    feat: singleton blocks

--- exploration/2308a_simpler_flanking/1_singleton_blocks.py
+++ exploration/2308a_simpler_flanking/1_singleton_blocks.py
@@ -52,6 +52,7 @@ sns.scatterplot(
     data=bdf.loc[accepted],
     x="len",
     y="count",
+    alpha=0.5,
 )
 plt.xscale("log")
 plt.xlabel("block length (bp)")
@@ -100,5 +101,193 @@ for bid in non_singleton:
     plt.tight_layout()
     plt.savefig(svfld / f"tree_{bid}.png", facecolor="white")
     plt.show()
+
+# %%
+
+bid = "NPEUXKFGMN"
+
+s = pan.blocks[bid].sequence
+# from Bio import SeqIO, Seq
+# seqfile = ut.expl_fld / "maps" / f"{bid}.fa"
+# with open(seqfile, "w") as f:
+#     seq = Seq.Seq(s)
+#     rec = SeqIO.SeqRecord(seq, id=bid, description="")
+#     SeqIO.write(rec, f, "fasta")
+print(s)
+# %%
+
+# %%
+# save PA pattern
+data_fld = ut.expl_fld / "singletons"
+data_fld.mkdir(exist_ok=True, parents=True)
+PA.index.name = "#name"
+PA = PA.applymap(lambda x: "-" if x == 0 else "P")
+PA.to_csv(data_fld / "PA.csv")
+
+
+# %%
+
+# infer mugration
+import os
+
+
+pa_inference = {}
+
+Bs = bdf.loc[non_singleton]
+Bs = Bs[Bs["len"] >= 500]
+Bs = Bs.index.to_list()
+for i, bid in enumerate(Bs):
+    print(f"processing {bid} \t - \t {i+1}/{len(Bs)}")
+    out_dir = data_fld / bid
+
+    # run treetime mugration
+    states = data_fld / "PA.csv"
+
+    cmd = f"""
+    treetime mugration \
+        --tree {ut.named_tree_file} \
+        --states {states} \
+        --attribute {bid} \
+        --outdir {out_dir} \
+    """
+
+    os.system(cmd)
+
+    # output file names
+    nexus_file = out_dir / "annotated_tree.nexus"
+    rates_file = out_dir / "GTR.txt"
+
+    # parse rates
+    rates = ut.parse_gtr(out_dir / "GTR.txt")
+
+    # parse inferred presence/absence pattern
+    pa_pattern = ut.parse_nexus(nexus_file)
+
+    os.system(f"rm -r {out_dir}")
+
+    pa_inference[bid] = {
+        "pa_pattern": pa_pattern,
+        "rates": rates,
+    }
+
+# write to json
+import json
+
+pa_inference_file = data_fld / "infer_pa.json"
+with open(pa_inference_file, "w") as f:
+    json.dump(pa_inference, f)
+
+# %%
+leaves_names = pan.strains()
+df = []
+for bid in Bs:
+    res = {"bid": bid}
+
+    info = pa_inference[bid]
+    pa = info["pa_pattern"]["pa"]
+    ev = info["pa_pattern"]["events"]
+
+    # number of leaves with/without the block
+    vals = np.array([pattern for node, pattern in pa.items() if node in leaves_names])
+    res["without"] = (vals == "-").sum()
+    res["with"] = (vals != "-").sum()
+    res["contexts"] = len(set(vals) - set(["-"]))
+
+    # number of events
+    g, l, m = 0, 0, 0
+    for node, e in ev:
+        if e == "gain":
+            g += 1
+        elif e == "loss":
+            l += 1
+    res["gain"] = g
+    res["loss"] = l
+
+    df.append(res)
+
+df = pd.DataFrame(df).set_index("bid")
+# %%
+
+import matplotlib as mpl
+
+
+def plot_tree_events(tree, pa_inference, bid, ax):
+    info = pa_inference[bid]
+    pa = info["pa_pattern"]["pa"]
+    ev = info["pa_pattern"]["events"]
+
+    cm = iter(mpl.cm.tab10.colors)
+
+    def next_color():
+        return next(cm)
+
+    adj_color = defaultdict(next_color)
+
+    def color_tree(node):
+        for nn, et in ev:
+            if node.name == nn:
+                if et == "gain":
+                    node.color = "lime"
+                elif et == "loss":
+                    node.color = "red"
+                else:
+                    node.color = "orange"
+        if node.color is None:
+            node.color = "black"
+        for c in node.clades:
+            color_tree(c)
+
+    def label_tree(node):
+        if node.is_terminal():
+            return node.name
+        else:
+            return ""
+
+    def lab_colors(nn):
+        if len(nn) == 0:
+            return None
+        if pa[nn] == "-":
+            return "lightgray"
+        else:
+            return adj_color[pa[nn]]
+
+    color_tree(tree.root)
+
+    Phylo.draw(
+        tree, label_func=label_tree, label_colors=lab_colors, axes=ax, do_show=False
+    )
+    plt.title(f"block - {bid}")
+
+
+PA = pan.to_blockcount_df()[Bs]
+for bid in Bs:
+    pa = PA[bid]
+    tree = ut.load_tree()
+    fig, ax = plt.subplots(figsize=(10, 10))
+    plot_tree_events(tree, pa_inference, bid, ax)
+    plt.tight_layout()
+    plt.savefig(svfld / f"tree_{bid}.png", facecolor="white")
+    plt.show()
+# %%
+
+x = df["without"]
+y = df["gain"] + df["loss"]
+plt.scatter(x, y, alpha=0.5)
+plt.plot([0, 10], [0, 10], "--", color="gray")
+plt.xticks(range(0, 22, 2))
+plt.xlabel("number of strains without the block")
+plt.ylabel("number of events")
+plt.grid(alpha=0.3)
+plt.tight_layout()
+plt.savefig(svfld / "events_vs_without.png", facecolor="white")
+plt.show()
+
+# %%
+cdf = df.merge(bdf, left_index=True, right_index=True)
+print(
+    cdf[["without", "count", "len", "gain", "loss"]]
+    .sort_values("len", ascending=False)
+    .to_markdown()
+)
 
 # %%
--- exploration/2308a_simpler_flanking/utils.py
+++ exploration/2308a_simpler_flanking/utils.py
@@ -1,4 +1,5 @@
 import pypangraph as pp
+import itertools as itt
 import pathlib
 from Bio import Phylo
 
@@ -10,6 +11,7 @@ tree_file = pg_fld / "asm20-100-5-filtered-coretree.nwk"
 
 expl_fld = pathlib.Path(__file__).parent.absolute() / "data"
 fig_fld = pathlib.Path(__file__).parent.absolute() / "figs"
+named_tree_file = expl_fld / "named_tree.nwk"
 
 
 def load_pangraph():
@@ -17,7 +19,7 @@ def load_pangraph():
 
 
 def load_tree():
-    return Phylo.read(tree_file, "newick")
+    return Phylo.read(named_tree_file, "newick")
 
 
 class Node:
@@ -197,3 +199,106 @@ def to_core_adjacencies(nodes, is_core):
         adj.append(j)
 
     return adj
+
+
+def parse_gtr(fname):
+    with open(fname, "r") as f:
+        lines = f.readlines()
+
+    info = {}
+
+    def find_subsequent(beg):
+        res = []
+        keep = False
+        for l in lines:
+            if l.startswith(beg):
+                keep = True
+            if len(l.strip()) == 0:
+                keep = False
+            if keep:
+                res.append(l.strip())
+        return res
+
+    def parse_matrix(mp, mat):
+        c1 = mat[0].split("\t")
+        c1 = [mp[c] for c in c1]
+        c2 = [mp[m.split("\t")[0]] for m in mat[1:]]
+        r1 = [float(m) for m in mat[1].split("\t")[1:]]
+        r2 = [float(m) for m in mat[2].split("\t")[1:]]
+        r = [r1, r2]
+        M = {}
+        for i, j in itt.product([0, 1], [0, 1]):
+            lab = f"{c2[i]}{c1[j]}"
+            M[lab] = r[i][j]
+        return M
+
+    # map
+    mp = find_subsequent("Character to attribute mapping")[1:]
+    mp = {m.split(":")[0]: m.split(":")[1].strip() for m in mp}
+    info["map"] = mp
+
+    # substitution rate
+    sr = find_subsequent("Substitution rate")[0].split(" ")[-1]
+    info["substitution_rate"] = float(sr)
+
+    # equilibrium freq
+    ef = find_subsequent("Equilibrium frequencies")[1:]
+    ef = {mp[e.split(":")[0]]: float(e.split(":")[1].strip()) for e in ef}
+    info["eq_freq"] = ef
+
+    # symmetrized rates
+    sr = find_subsequent("Symmetrized rates from")[1:]
+    info["symm_rates"] = parse_matrix(mp, sr)
+
+    # actual rates
+    ar = find_subsequent("Actual rates from")[1:]
+    info["act_rates"] = parse_matrix(mp, ar)
+
+    return info
+
+
+def parse_nexus(fname):
+    pa_pattern = {}
+    node_name = {}
+
+    # load nexus tree
+    tree = Phylo.read(fname, "nexus")
+
+    # extract presence/absence pattern
+    def parse_prop(n):
+        prop = n.comment
+        return prop.split('"')[-2]
+
+    for n in tree.get_terminals():
+        node_name[n] = n.name
+        pa_pattern[n.name] = parse_prop(n)
+
+    for n in tree.get_nonterminals():
+        node_name[n] = n.confidence
+        pa_pattern[n.confidence] = parse_prop(n)
+
+    # find gain-loss events
+    events = []
+
+    def find_events(parent):
+        if parent.is_terminal():
+            return
+
+        pn = node_name[parent]
+        ps = pa_pattern[pn]
+        for child in parent.clades:
+            cn = node_name[child]
+            cs = pa_pattern[cn]
+            if cs != ps:
+                if ps == "-":
+                    events.append((cn, "gain"))
+                elif cs == "-":
+                    events.append((cn, "loss"))
+                else:
+                    events.append((cn, f"{cs}|{ps}"))
+            find_events(child)
+
+    find_events(tree.root)
+
+    info = {"pa": pa_pattern, "events": events}
+    return info
