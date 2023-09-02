c389b5eaf0275b4a1bcf7d33413adb43cc024d38
Marco Molari <mmolari91@gmail.com>
2023-09-02 14:34:22 +0200

    feat: core genome synteny figure

---
+++ exploration/2309a_core_synteny/1_core_sinteny.py
@@ -0,0 +1,259 @@
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pypangraph as pp
+from Bio import Phylo
+from collections import Counter, defaultdict
+import argparse
+import pathlib
+import utils as ut
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="""Given a pangraph, generates a list of core edges and their frequencies.
+        Only core-blocks longer than the specified threshold are considered.
+        """
+    )
+    parser.add_argument("--pangraph", type=str, required=True)
+    parser.add_argument("--tree", type=str, required=True)
+    parser.add_argument("--fig_synt", type=str, required=True)
+    parser.add_argument("--fig_tree", type=str, required=True)
+    parser.add_argument("--fig_blocks", type=str, required=True)
+    return parser.parse_args()
+
+
+fig_fld = pathlib.Path(
+    "/home/marco/ownCloud/neherlab/code/pangenome-evo/exploration/2309a_core_synteny/figs"
+)
+pg_file = "/home/marco/ownCloud/neherlab/code/pangenome-evo/results/ST131/pangraph/asm20-100-5-polished.json"
+tree_file = "/home/marco/ownCloud/neherlab/code/pangenome-evo/results/ST131/pangraph/asm20-100-5-filtered-coretree.nwk"
+svfig_synt = fig_fld / "core_synteny.pdf"
+svfig_tree = fig_fld / "tree.pdf"
+svfig_blocks = fig_fld / "blocks.pdf"
+
+# %%
+pan = pp.Pangraph.load_json(pg_file)
+bdf = pan.to_blockstats_df()
+# %%
+
+
+def keep_f(bid):
+    return bdf.loc[bid, "core"]
+
+
+paths = ut.pangraph_to_path_dict(pan)
+paths = ut.filter_paths(paths, keep_f)
+bdf = bdf[bdf["core"]].copy().drop(["core", "duplicated", "n. strains"], axis=1)
+
+# %%
+
+
+def path_edge_count(paths):
+    """Count internal edges of paths"""
+    ct = Counter()
+    for iso, p in paths.items():
+        L = len(p.nodes)
+        for i in range(L):
+            e = ut.Edge(p.nodes[i], p.nodes[(i + 1) % L])
+            ct.update([e])
+    return dict(ct)
+
+
+def find_mergers(edge_ct, block_ct):
+    """Create a dictionary source -> sinks of block-ids to be merged"""
+    mergers = {}
+    for e, ec in edge_ct.items():
+        bl, br = e.left.id, e.right.id
+        if (ec == block_ct[bl]) and (ec == block_ct[br]):
+            # merge
+            if bl in mergers:
+                mergers[br] = mergers[bl]
+            elif br in mergers:
+                mergers[bl] = mergers[br]
+            else:
+                mergers[br] = bl
+    return mergers
+
+
+def perform_mergers(mergers, paths, block_df):
+    """Performs selected mergers in paths and block stats"""
+    for source, sink in mergers.items():
+        # modify block count dataframe
+        block_df.loc[sink, "len"] += block_df["len"][source]
+        assert block_df["count"][sink] == block_df["count"][source]
+        block_df.drop(source, inplace=True)
+
+    removed_blocks = set(mergers.keys()) - set(mergers.values())
+
+    def keep(bid):
+        return bid not in removed_blocks
+
+    return ut.filter_paths(paths, keep)
+
+
+# %%
+ec = path_edge_count(paths)
+mg = find_mergers(ec, bdf["count"].to_dict())
+paths = perform_mergers(mg, paths, bdf)
+
+# %%
+longest_bid = bdf["len"].sort_values().index[-1]
+
+
+def roll_to_anchor(paths, anchor):
+    """Rolls the path so that the anchor is the first node"""
+    anchor_node = ut.Node(anchor, True)
+    for iso, path in paths.items():
+        if not (anchor_node in path.nodes):
+            path = path.invert()
+        assert anchor_node in path.nodes
+
+        idx = path.nodes.index(anchor_node)
+        path.nodes = path.nodes[idx:] + path.nodes[:idx]
+        paths[iso] = path
+
+
+roll_to_anchor(paths, longest_bid)
+
+# %%
+
+path_cats = defaultdict(list)
+for iso, path in paths.items():
+    path_cats[path].append(iso)
+
+path_cats = dict(path_cats)
+print(f"n. different paths = {len(path_cats)}")
+abundances = [len(isos) for isos in path_cats.values()]
+print(f"abundances = {abundances}")
+common_path = max(path_cats, key=lambda p: len(path_cats[p]))
+strand_common = {node.id: node.strand for node in common_path.nodes}
+# %%
+
+fig, ax = plt.subplots(figsize=(10, 10))
+
+y = 0
+# cmap = plt.cm.get_cmap("tab20b")(range(len(bdf)))
+cmap = plt.cm.get_cmap("nipy_spectral")(np.linspace(0, 1, len(bdf)))
+colors = defaultdict(lambda: cmap[len(colors)])
+
+
+def cmap_iso_generator():
+    cm = plt.cm.get_cmap("tab20")
+    i = 0
+    while True:
+        yield cm(i)
+        i += 1
+
+
+cmap_iso = cmap_iso_generator()
+
+
+iso_color = {}
+xpos = defaultdict(list)
+ylabels = []
+yticks = []
+for path, isolates in path_cats.items():
+    for i, node in enumerate(path.nodes):
+        xpos[node.id].append(i + 0.5)
+        bid = node.id
+        c = colors[bid]
+        st = node.strand if strand_common[bid] else not node.strand
+        ax.barh(
+            y,
+            1,
+            left=i,
+            color=c,
+            label=bid,
+            edgecolor="dimgray",
+            hatch=None if st else "///",
+            height=0.3 + 0.5 * (bdf.loc[bid, "len"] > 100000),
+        )
+
+    if len(isolates) > 2:
+        yl = f"n = {len(isolates)}"
+    else:
+        yl = ", ".join(isolates)
+
+    if len(isolates) > 10:
+        ic = "k"
+    else:
+        ic = next(cmap_iso)
+        for iso in isolates:
+            iso_color[iso] = ic
+    ax.annotate(
+        yl,
+        xy=(0, y),
+        xytext=(-10, 0),
+        textcoords="offset points",
+        ha="right",
+        va="center",
+        color=ic,
+    )
+
+    y -= 1
+
+for bid, X in xpos.items():
+    ax.plot(X, -np.arange(len(X)), "k", lw=0.5, zorder=-1)
+
+# ax.set_yticks(yticks)
+# ax.set_yticklabels(ylabels)
+ax.set_yticks([])
+ax.set_xlim(-10, len(common_path.nodes) + 1)
+
+ax.set_xticks([])
+for spine in ax.spines.values():
+    spine.set_visible(False)
+
+plt.tight_layout()
+plt.savefig(svfig_synt)
+plt.show()
+
+# %%
+
+tree = Phylo.read(tree_file, "newick")
+
+fig, ax = plt.subplots(figsize=(3.5, 10))
+
+
+Phylo.draw(
+    tree,
+    axes=ax,
+    do_show=False,
+    show_confidence=False,
+    label_func=lambda x: x.name if x.name in iso_color else "",
+    label_colors=lambda name: iso_color[name] if name in iso_color else "k",
+)
+sns.despine()
+plt.tight_layout()
+plt.savefig(svfig_tree)
+plt.show()
+
+# %%
+
+# block lengths
+
+fig, ax = plt.subplots(figsize=(10, 2))
+
+y = 0
+yticks, ylabels = [], []
+for node in common_path.nodes:
+    bid = node.id
+    L = bdf.loc[bid, "len"]
+    ax.bar(y, L, color=colors[bid], edgecolor="dimgray")
+    ylabels.append(f"{int(np.round(L/1000))} kbp")
+    yticks.append(y)
+    y += 1
+ax.set_xticks(yticks)
+ax.set_xticklabels(ylabels, rotation=90)
+ax.set_ylim(1e3, 1e6)
+ax.set_yscale("log")
+sns.despine()
+ax.grid(which="major", axis="y", alpha=0.6)
+# ax.grid(which="minor", axis="y", alpha=0.3)
+plt.tight_layout()
+plt.savefig(svfig_blocks)
+plt.show()
+
+# %%
---
+++ exploration/2309a_core_synteny/utils.py
@@ -0,0 +1,163 @@
+class Node:
+    """Combination of block id and strandedness"""
+
+    def __init__(self, bid: str, strand: bool) -> None:
+        self.id = bid
+        self.strand = strand
+
+    def invert(self) -> "Node":
+        return Node(self.id, not self.strand)
+
+    def __eq__(self, other: object) -> bool:
+        return self.id == other.id and self.strand == other.strand
+
+    def __hash__(self) -> int:
+        return hash((self.id, self.strand))
+
+    def __repr__(self) -> str:
+        s = "+" if self.strand else "-"
+        return f"[{self.id}|{s}]"
+
+    def to_str_id(self):
+        s = "f" if self.strand else "r"
+        return f"{self.id}_{s}"
+
+    @staticmethod
+    def from_str_id(t) -> "Node":
+        bid = t.split("_")[0]
+        strand = True if t.split("_")[1] == "f" else False
+        return Node(bid, strand)
+
+
+class Path:
+    """A path is a list of nodes"""
+
+    def __init__(self, nodes=[]) -> None:
+        self.nodes = nodes
+
+    def add_left(self, node: Node) -> None:
+        self.nodes.insert(0, node)
+
+    def add_right(self, node: Node) -> None:
+        self.nodes.append(node)
+
+    def invert(self) -> "Path":
+        return Path([n.invert() for n in self.nodes[::-1]])
+
+    def __eq__(self, o: object) -> bool:
+        return self.nodes == o.nodes
+
+    def __hash__(self) -> int:
+        return hash(tuple(self.nodes))
+
+    def __repr__(self) -> str:
+        return "_".join([str(n) for n in self.nodes])
+
+    def __len__(self) -> int:
+        return len(self.nodes)
+
+    def to_list(self):
+        return [n.to_str_id() for n in self.nodes]
+
+    @staticmethod
+    def from_list(path_list) -> "Path":
+        return Path([Node.from_str_id(nid) for nid in path_list])
+
+
+class Edge:
+    """Oriented link between two nodes/paths"""
+
+    def __init__(self, left, right) -> None:
+        self.left = left
+        self.right = right
+
+    def invert(self) -> "Edge":
+        return Edge(self.right.invert(), self.left.invert())
+
+    def __side_eq__(self, o: object) -> bool:
+        return self.left == o.left and self.right == o.right
+
+    def __eq__(self, o: object) -> bool:
+        return self.__side_eq__(o) or self.__side_eq__(o.invert())
+
+    def __side_hash__(self) -> int:
+        return hash((self.left, self.right))
+
+    def __hash__(self) -> int:
+        return self.__side_hash__() ^ self.invert().__side_hash__()
+
+    def __repr__(self) -> str:
+        return f"{self.left} <--> {self.right}"
+
+    def to_str_id(self) -> list:
+        return "__".join([self.left.to_str_id(), self.right.to_str_id()])
+
+    @staticmethod
+    def from_str_id(t) -> "Edge":
+        left, right = t.split("__")
+        return Edge(Node.from_str_id(left), Node.from_str_id(right))
+
+
+class Junction:
+    """A junction is a combination of a node (or path) flanked by two nodes, with reverse-complement simmetry"""
+
+    def __init__(self, left: Node, center: Path, right: Node) -> None:
+        self.left = left
+        self.center = center
+        self.right = right
+
+    def invert(self) -> "Junction":
+        return Junction(self.right.invert(), self.center.invert(), self.left.invert())
+
+    def flanks_bid(self, bid) -> bool:
+        return (self.left.id == bid) or (self.right.id == bid)
+
+    def __side_eq__(self, o: object) -> bool:
+        return self.left == o.left and self.center == o.center and self.right == o.right
+
+    def __eq__(self, o: object) -> bool:
+        return self.__side_eq__(o) or self.__side_eq__(o.invert())
+
+    def __side_hash__(self) -> int:
+        return hash((self.left, self.center, self.right))
+
+    def __hash__(self) -> int:
+        return self.__side_hash__() ^ self.invert().__side_hash__()
+
+    def __repr__(self) -> str:
+        return f"{self.left} <-- {self.center} --> {self.right}"
+
+    def to_list(self):
+        return [self.left.to_str_id(), self.center.to_list(), self.right.to_str_id()]
+
+    @staticmethod
+    def from_list(t) -> "Junction":
+        return Junction(
+            Node.from_str_id(t[0]), Path.from_list(t[1]), Node.from_str_id(t[2])
+        )
+
+
+def pangraph_to_path_dict(pan):
+    """Creates a dictionary isolate -> path objects"""
+    res = {}
+    for path in pan.paths:
+        name = path.name
+        B = path.block_ids
+        S = path.block_strands
+        nodes = [Node(b, s) for b, s in zip(B, S)]
+        res[name] = Path(nodes)
+    return res
+
+
+def filter_paths(paths, keep_f):
+    """Given a filter function, removes nodes that fail the condition from
+    the path dictionaries."""
+    res = {}
+    for iso, path in paths.items():
+        filt_path = Path([node for node in path.nodes if keep_f(node.id)])
+        res[iso] = filt_path
+    return res
+
+
+def merge_transitive_edges(paths, stats_df):
+    pass
