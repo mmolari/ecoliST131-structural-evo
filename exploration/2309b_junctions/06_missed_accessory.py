e7a512b48be7c2cf8b1361e8d39f4a1a58a79af4
Marco Molari <mmolari91@gmail.com>
2023-10-26 18:44:07 +0200

    feat: check IS

--- exploration/2309b_junctions/06_missed_accessory.py
+++ exploration/2309b_junctions/06_missed_accessory.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pypangraph as pp
 import utils as ut
+from collections import defaultdict
+
 
 len_thr = 500
 dset = "ST131_full"
@@ -15,170 +17,370 @@ pangraph_file = f"../../results/{dset}/pangraph/asm20-100-5-polished.json"
 pan = pp.Pangraph.load_json(pangraph_file)
 bdf = pan.to_blockstats_df()
 paths = ut.pangraph_to_path_dict(pan)
-del pan
 edges_df = pd.read_csv(edge_file)
 
 # %%
 
 
-def encompassing_edges(path, core_f):
-    res = {}
-
-    first_bid = None
-    done = False
-    L = len(path)
-    for i, node in enumerate(path.nodes):
-        if core_f(node.id):
-            first_bid = node.id
-            break
-    edge = ut.Edge(path.nodes[i], None)
-    link = []
-    i += 1
-    while not done:
-        node = path.nodes[i]
-        is_core = core_f(node.id)
-        if is_core:
-            edge.right = node
-            res[edge] = link
-            link = []
-            edge = ut.Edge(node, None)
-            if node.id == first_bid:
-                done = True
-        else:
-            link.append(node)
-        i += 1
-        i %= L
-    return res
+def is_core(node_id):
+    return (bdf.loc[node_id, "len"] >= len_thr) and bdf.loc[node_id, "core"]
 
 
-def core_f(block_id):
-    keep = bdf.loc[block_id, "core"]
-    keep &= bdf.loc[block_id, "len"] >= len_thr
-    return keep
-
-
-links = {}
+# core-junctions dataframe
+jdf = []
 for iso, path in paths.items():
-    links[iso] = encompassing_edges(path, core_f)
+    junctions = ut.path_junction_split(path, is_core)
+    for J in junctions:
+        edge = J.flanking_edge()
+        L = sum(bdf.loc[node.id, "len"] for node in J.center.nodes)
+        jdf.append({"iso": iso, "edge": edge.to_unique_str_id(), "len": L})
+jdf = pd.DataFrame(jdf)
+jdf = jdf.pivot_table(index="iso", columns="edge", values="len")
+jdf
 # %%
 
-full_df = []
-for iso, link in links.items():
-    for edge, nodes in link.items():
-        res = {}
-        L = sum([bdf.loc[n.id, "len"] for n in nodes], start=0)
-        res["edge"] = edge.to_unique_str_id()
-        res["iso"] = iso
-        res["len"] = L
-        res["n_nodes"] = len(nodes)
-        full_df.append(res)
-full_df = pd.DataFrame(full_df)
-df = full_df.pivot(index="iso", columns="edge", values="len")
-col_ord = df.notna().sum().sort_values(ascending=False).index
-df = df[col_ord]
-df
-
-# %%
-
-core_edges_1 = df.columns[df.notna().sum() == len(paths)].to_numpy()
-core_edges_2 = edges_df[edges_df["count"] == len(paths)]["edge"].to_numpy()
-
-ce1 = set([ut.Edge.from_str_id(e) for e in core_edges_1])
-ce2 = set([ut.Edge.from_str_id(e) for e in core_edges_2])
-
-assert ce1 == ce2
-# %%
-
-df_core = df[core_edges_1].copy()
-df_acc = df.drop(core_edges_1, axis=1).copy()
-# %%
-fig, ax = plt.subplots(figsize=(20, 30))
-sns.heatmap(df_acc.notna(), cmap="Blues", ax=ax)
-plt.show()
-
-
-fig, ax = plt.subplots(figsize=(20, 30))
-sns.heatmap(df_acc, cmap="rainbow", ax=ax)
-plt.show()
-# %%
-
-sns.histplot(df_core.mean(), log_scale=True, bins=50)
-# plt.xscale("log")
-plt.show()
-# %%
-sns.histplot(x=(df_core == 0).sum(), y=df_core.mean(), log_scale=(False, True), bins=50)
-# %%
-
-n_zero_core = (df_core == 0).sum()
-avg_nonzero_len_core = df_core[df_core > 0].mean()
-avg_nonzero_len_core.fillna(0, inplace=True)
-
-x_bins = np.linspace(0, len(paths) + 1, 50)
-y_bins = np.logspace(
-    np.log10(avg_nonzero_len_core.min()), np.log10(avg_nonzero_len_core.max()), 50
+# verify compatibility with edge file
+old_edge_df = pd.read_csv(edge_file)
+old_core_edges = old_edge_df[old_edge_df["count"] == len(paths)]["edge"].values
+old_core_edges = set(
+    [ut.Edge.from_str_id(e).to_unique_str_id() for e in old_core_edges]
 )
-sns.jointplot(
-    x=n_zero_core,
-    y=avg_nonzero_len_core,
-    kind="hist",
-    joint_kws={
-        "bins": (x_bins, y_bins),
-        # "log_scale": (False, True),
-    },
-)
-plt.yscale("log")
-plt.show()
-# %%
-is_core = df.notna().sum() == len(paths)
-is_core.value_counts()
-# %%
-core_acc_sequence = df.sum()[is_core].sum() / len(paths)
-acc_acc_sequence = df.sum()[~is_core].sum() / len(paths)
-
-print(f"Core-acc sequence: {core_acc_sequence/1e6} Mbp")
-print(f"Acc-acc sequence: {acc_acc_sequence/1e6} Mbp")
+new_core_edges = set(jdf.columns[jdf.notna().sum() == len(paths)])
+assert old_core_edges == new_core_edges
 
 # %%
-from collections import defaultdict
 
-imputable = defaultdict(float)
-n_breakpoints = defaultdict(int)
-full_strain_set = set(paths.keys())
-for col in df.columns:
-    srs = df[col]
-    strains_with = set(srs[srs.notna()].index)
-    strains_without = full_strain_set - strains_with
+# select core-edges
+core_mask = jdf.notna().sum() == len(paths)
+core_edges = jdf.columns[core_mask]
+jdf_core = jdf[core_edges]
 
-    if len(strains_with) > len(strains_without):
-        culprit = strains_without
-    else:
-        culprit = strains_with
+# select accessory-edges
+acc_edges = jdf.columns[~core_mask]
+jdf_acc = jdf[acc_edges]
 
-    sequence = df[col].sum()
-    for iso in culprit:
-        imputable[iso] += sequence / len(culprit)
-        n_breakpoints[iso] += 1
-imputable = pd.Series(imputable)
-n_breakpoints = pd.Series(n_breakpoints)
-imputable
+
+# relative genome size
+core_genome = jdf_core.sum().sum() / len(paths)
+acc_genome = jdf_acc.sum().sum() / len(paths)
+print(f"accessory genome in backbone: {core_genome/1e6:.2f} Mbp")
+print(f"accessory genome out of backbone: {acc_genome/1e6:.2f} Mbp")
+print(f"n. backbone edges {len(core_edges)}")
+print(f"n. off-backbone edges {len(acc_edges)}")
+
+
 # %%
-fig, axs = plt.subplots(2, 1, figsize=(5, 10), sharex=True)
+def plot_core_len_vs_frequency(core_df):
+    # evaluate average length of non-empty junctions
+    avg_len = core_df[core_df > 0].mean(axis=0)
+    avg_len.fillna(0, inplace=True)
+    freq = (core_df > 0).sum(axis=0) / len(core_df.index)
+    g = sns.jointplot(
+        x=freq,
+        y=avg_len,
+        height=8,
+        kind="hist",
+        joint_kws={"bins": (50), "log_scale": (False, True)},
+        marginal_kws={"bins": 50},
+    )
+    g.set_axis_labels("non-empty frequency", "average length of non-empty junctions")
+    # add grid to the joint distribution
+    g.ax_joint.grid(True, alpha=0.3)
 
-order = imputable.sort_values(ascending=False).index
-ax = axs[0]
-ax.plot(imputable[order] / len(paths), "o")
-ax.set_yscale("log")
 
-ax = axs[1]
-ax.plot(n_breakpoints[order], "o")
-ax.set_yscale("log")
-
-for ax in axs:
-    ax.set_xticks(range(len(imputable)))
-    ax.set_xticklabels(imputable.sort_values(ascending=False).index, rotation=90)
-    ax.grid(alpha=0.3)
-
+plot_core_len_vs_frequency(jdf_core)
 plt.tight_layout()
 plt.show()
 
+# %%
+
+
+def plot_accessory_vs_core(df_acc, df_core):
+    # evaluate average length of non-empty junctions
+    avg_len_core = df_core.sum(axis=0) / df_core.shape[0]
+    avg_len_core.fillna(0, inplace=True)
+    avg_len_acc = df_acc.sum(axis=0) / df_acc.shape[0]
+    avg_len_acc.fillna(0, inplace=True)
+    plt.hist(
+        [avg_len_core, avg_len_acc],
+        bins=50,
+        label=["core", "accessory"],
+        log=True,
+        histtype="stepfilled",
+        alpha=0.3,
+        edgecolor="k",
+    )
+    # plt.xscale("log")
+    plt.legend()
+    plt.xlabel("total amount of accessory genome per isolate (bp)")
+    plt.show()
+
+
+plot_accessory_vs_core(jdf_acc, jdf_core)
+plt.tight_layout()
+plt.show()
+
+# %%
+
+
+def plot_offbackbone_PA(dfa):
+    consensus = jdf_acc.isna().mode().T[0]
+    iso_mask = (consensus == jdf_acc.isna()).all(axis=1)
+    sdf = dfa.loc[~iso_mask].notna()
+    order = sdf.sum(axis=0).sort_values(ascending=False).index
+    sdf = sdf[order]
+    fig, ax = plt.subplots(figsize=np.array(sdf.shape) * 0.18 + np.array([3, 1]))
+    sns.heatmap(sdf.T, cbar=True, ax=ax, cmap="GnBu", cbar_kws={"shrink": 0.5})
+    # add title
+    ax.set_title("presence/absence of off-backbone core-genome edges")
+    plt.tight_layout()
+    plt.show()
+
+
+plot_offbackbone_PA(jdf_acc)
+
+
+# %%
+def impute(dfa):
+    idf = defaultdict(lambda: {"imputable": 0, "n_breakpoints": 0})
+    full_strain_set = set(paths.keys())
+    for col in dfa.columns:
+        srs = dfa[col]
+        strains_with = set(srs[srs.notna()].index)
+        strains_without = full_strain_set - strains_with
+
+        if len(strains_with) > len(strains_without):
+            culprit = strains_without
+        else:
+            culprit = strains_with
+
+        sequence = dfa[col].sum()
+        for iso in culprit:
+            idf[iso]["imputable"] += sequence / len(culprit)
+            idf[iso]["n_breakpoints"] += 1
+    idf = pd.DataFrame(idf).T
+    return idf
+
+
+def plot_impute_accessory(dfa):
+    # impute missing accessory genome
+    idf = impute(dfa)
+    idf = idf.sort_values("imputable", ascending=False)
+
+    fig, axs = plt.subplots(
+        2,
+        1,
+        figsize=(idf.shape[0] * 0.2 + 1, 8),
+        sharex=True,
+    )
+    ax = axs[0]
+    h = idf["imputable"] / len(paths)
+    bpl = ax.bar(range(len(idf)), h, color="C1")
+    ax.bar_label(
+        bpl,
+        labels=[f"{np.round(x/1e3):.0f} kbp" for x in h],
+        rotation=90,
+        padding=5,
+    )
+    ax.set_ylabel("imputable accessory sequence loss (bp)")
+    ax.set_yscale("log")
+
+    ax = axs[1]
+    bpl = ax.bar(range(len(idf)), idf["n_breakpoints"], color="C1")
+    ax.bar_label(
+        bpl,
+        labels=[f"{x:.0f}" for x in idf["n_breakpoints"]],
+        rotation=90,
+        padding=5,
+    )
+    ax.set_ylabel("n. backbone breakpoints")
+    ax.set_yscale("log")
+
+    for ax in axs:
+        ax.set_xticks(range(len(idf)))
+        ax.set_xticklabels(
+            idf.sort_values("imputable", ascending=False).index, rotation=90
+        )
+        ax.grid(alpha=0.3)
+
+    fig.suptitle("off-backbone edges")
+    sns.despine(fig)
+    plt.tight_layout()
+    plt.show()
+
+
+plot_impute_accessory(jdf_acc)
+
+# %%
+
+
+def plot_offbackbone_frequency(dfa):
+    # bimodal frequency, rare and common
+    notna = dfa.notna().sum() / len(dfa.index)
+    fig, ax = plt.subplots(1, 1, figsize=(10, 3))
+    bins = (np.arange(len(dfa.index) + 2) - 0.5) / len(dfa.index)
+    plt.hist(notna, bins=bins, color="C1")
+    # add minor xticks every 0.05
+    ax.set_xticks(np.arange(0, 1.01, 0.05), minor=True)
+    ax.set_xticks(np.arange(0, 1.01, 0.1), minor=False)
+
+    plt.xlabel("frequency of off-backbone edges")
+    plt.ylabel("n. edges")
+    plt.show()
+
+
+plot_offbackbone_frequency(jdf_acc)
+
+# %%
+# check peak
+mean_nonempty_len = jdf_core[jdf_core > 0].mean()
+bins = np.logspace(1, 6, 100)
+h = plt.hist(
+    mean_nonempty_len,
+    log=True,
+    bins=bins,
+)
+plt.xscale("log")
+plt.show()
+# %%
+mask = mean_nonempty_len < 830
+mask &= mean_nonempty_len > 739
+mean_nonempty_len[mask].value_counts()
+# %%
+mask = mean_nonempty_len == 766
+suspicious = mean_nonempty_len[mask].index.to_numpy()
+# %%
+
+sus = []
+for iso, path in paths.items():
+    junctions = ut.path_junction_split(path, is_core)
+    for J in junctions:
+        edge = J.flanking_edge()
+        if not edge.to_unique_str_id() in suspicious:
+            continue
+        path = J.center
+        if edge.left.id > edge.right.id:
+            path = path.invert()
+        L_nodes = [bdf.loc[node.id, "len"] for node in path.nodes]
+        L = sum(L_nodes)
+        if L != 766:
+            continue
+        N_nodes = len(path.nodes)
+        sus.append(
+            {
+                "iso": iso,
+                "edge": edge.to_unique_str_id(),
+                "len": L,
+                "nodes": N_nodes,
+                "L_nodes": L_nodes,
+                "path": path,
+            }
+        )
+sus = pd.DataFrame(sus)
+sus = sus.sort_values(["edge", "iso"]).reset_index(drop=True)
+sus.to_csv("data/suspicious.csv")
+sus
+# %%
+# it's in many genomes
+sus["iso"].value_counts()
+# %%
+
+edge = "BDLHMQPIBW_f__NKOVRJRZTL_f"
+iso_in = "NZ_CP124372.1"
+iso_out = "NZ_CP124339.1"
+
+edge = ut.Edge.from_str_id(edge)
+bl = pan.blocks[edge.left.id]
+br = pan.blocks[edge.right.id]
+pos_l_in = [
+    (occ[0], occ[2], *pos) for occ, pos in bl.alignment.pos.items() if occ[0] == iso_in
+][0]
+pos_l_out = [
+    (occ[0], occ[2], *pos) for occ, pos in bl.alignment.pos.items() if occ[0] == iso_out
+][0]
+pos_r_in = [
+    (occ[0], occ[2], *pos) for occ, pos in br.alignment.pos.items() if occ[0] == iso_in
+][0]
+pos_r_out = [
+    (occ[0], occ[2], *pos) for occ, pos in br.alignment.pos.items() if occ[0] == iso_out
+][0]
+print(pos_l_in)
+print(pos_r_in)
+print(pos_l_out)
+print(pos_r_out)
+
+# extract sequence from iso_in
+from Bio import SeqIO
+
+with open(f"../../data/fa/{iso_in}.fa") as f:
+    genome_in = SeqIO.read(f, "fasta")
+if pos_l_in[3] < pos_r_in[2]:
+    seq_sus = genome_in.seq[pos_l_in[3] : pos_r_in[2]]
+else:
+    seq_sus = genome_in.seq[pos_r_in[3] : pos_l_in[2]]
+print(seq_sus)
+
+# %%
+
+import subprocess
+
+
+def disentangle_pos(pos_l, pos_r):
+    sdf = defaultdict(dict)
+    for occ, pos in pos_l.items():
+        sdf[occ[0]]["lb"] = pos[0]
+        sdf[occ[0]]["le"] = pos[1]
+        sdf[occ[0]]["ls"] = occ[2]
+    for occ, pos in pos_r.items():
+        sdf[occ[0]]["rb"] = pos[0]
+        sdf[occ[0]]["re"] = pos[1]
+        sdf[occ[0]]["rs"] = occ[2]
+    return pd.DataFrame(sdf).T
+
+
+def load_genome(iso):
+    with open(f"../../data/fa/{iso}.fa") as f:
+        genome = SeqIO.read(f, "fasta")
+    return genome.seq
+
+
+def extract_seq(seq, delta, pos):
+    rev_comp = pos["ls"]
+    avg_l = (pos["lb"] + pos["le"]) / 2
+    avg_r = (pos["rb"] + pos["re"]) / 2
+    assert np.abs(avg_l - avg_r) < 1e6, f"weird positions: {pos}"
+    fwd = avg_l < avg_r
+    beg = pos["le"] if fwd else pos["re"]
+    end = pos["rb"] if fwd else pos["lb"]
+    seq = seq[beg - delta : end + delta]
+    if not fwd:
+        seq = seq.reverse_complement()
+    return seq
+
+
+delta = 500
+for edge, sdf in sus.groupby("edge"):
+    print(sdf)
+
+    edge = ut.Edge.from_str_id(edge)
+    pos_l = pan.blocks[edge.left.id].alignment.pos
+    pos_r = pan.blocks[edge.right.id].alignment.pos
+    pos = disentangle_pos(pos_l, pos_r)
+    records = []
+    for iso, row in pos.iterrows():
+        genome = load_genome(iso)
+        seq = extract_seq(genome, delta, row)
+        records.append(SeqIO.SeqRecord(seq, id=iso))
+    fname = f"data/IS/{edge.to_unique_str_id()}.fa"
+    SeqIO.write(records, fname, "fasta")
+
+    # align usign mafft
+    fname_out = f"data/IS/{edge.to_unique_str_id()}.aln.fa"
+    subprocess.run(
+        f"mafft --quiet --auto {fname} > {fname_out}",
+        shell=True,
+    )
+    # remove unaligned file
+    subprocess.run(f"rm {fname}", shell=True)
+    break
 # %%
