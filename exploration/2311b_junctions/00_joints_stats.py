d889e622f1cd8ccec0a5bde30cf8176a1ba20f4b
Marco Molari <mmolari91@gmail.com>
2023-11-27 14:55:46 +0100

    chore: survey plots

--- exploration/2311b_junctions/00_joints_stats.py
+++ exploration/2311b_junctions/00_joints_stats.py
@@ -20,7 +20,7 @@ fig_fld.mkdir(exist_ok=True, parents=True)
 
 def svfig(name):
     # plt.savefig(fig_fld / f"{name}.svg")
-    plt.savefig(fig_fld / f"{name}.png", facecolor="white")
+    plt.savefig(fig_fld / f"{name}.png", facecolor="white", dpi=150)
 
 
 fld = pathlib.Path(f"../../results/{dset}")
@@ -56,6 +56,71 @@ cols = [
     "nonempty_acc_len",
     "nonempty_freq",
 ]
+
+# %% edge frequency
+
+freqs = df_el.notna().sum(axis=0).value_counts()
+
+fig, ax = plt.subplots(1, 1, figsize=(5, 5))
+sns.barplot(x=freqs.index, y=freqs.values, ax=ax, color="gray", log=True)
+ax.bar_label(ax.containers[0], fmt="%d")
+ax.set_title("edge frequency")
+ax.set_xlabel("n. isolates")
+ax.set_ylabel("n. edges")
+plt.tight_layout()
+svfig("edge_freq")
+plt.show()
+
+# %% edge length and occupation frequency, stratified by category
+
+
+def edge_cat(freq):
+    if freq < 0.1:
+        return "rare"
+    elif freq < 0.9:
+        return "intermediate"
+    elif freq < 1.0:
+        return "common"
+    elif freq == 1.0:
+        return "backbone"
+    else:
+        raise ValueError(f"invalid freq: {freq}")
+
+
+counts = df_el.notna().sum(axis=0)
+cats = (counts / df_el.shape[0]).apply(edge_cat)
+nonempty_l = df_el.mean(axis=0)
+nonempty_freq = (df_el > 0).sum(axis=0) / counts
+sdf = pd.DataFrame(
+    {"nonempty_freq": nonempty_freq, "cat": cats, "nonempty_acc_len": nonempty_l},
+    index=nonempty_freq.index,
+)
+
+bins = (np.linspace(0, 1, 51), [-0.5, 0.5] + list(np.logspace(0, 6, 51)))
+g = sns.displot(
+    data=sdf,
+    x="nonempty_freq",
+    y="nonempty_acc_len",
+    kind="hist",
+    bins=bins,
+    col="cat",
+    height=4,
+    aspect=0.8,
+)
+for ax in g.axes.flat:
+    ax.set_yscale("symlog", linthresh=1)
+    ax.set_ylim(bottom=0)
+    # ax.set_xlim(left=0)
+    ax.set_xlabel("edge occupation frequency")
+    ax.set_ylabel("occupied edge average length (bp)")
+    title = ax.get_title().split("=")[1].strip()
+    title += f" (n={(cats == title).sum()})"
+    ax.set_title(title)
+    ax.grid(alpha=0.3)
+
+plt.tight_layout()
+svfig("edge_cats")
+plt.show()
 # %%
 fig = plt.figure(figsize=(10, 10))
 sns.scatterplot(
