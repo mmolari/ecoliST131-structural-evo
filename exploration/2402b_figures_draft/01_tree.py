f9dfae61715f60ce1723024c07fcb9457dface87
Marco Molari <mmolari91@gmail.com>
2024-03-06 10:52:47 +0100

    feat: draft fig tree

--- exploration/2402b_figures_draft/01_tree.py
+++ exploration/2402b_figures_draft/01_tree.py
@@ -12,127 +12,146 @@ fig_fld = pathlib.Path("figs/f01")
 fig_fld.mkdir(parents=True, exist_ok=True)
 
 # %%
-tree_file = "../../results/ST131_ABC/pangraph/asm20-100-5-filtered-coretree.nwk"
-tree = Phylo.read(tree_file, "newick")
-strains = [x.name for x in tree.get_terminals()]
-strain_y = {s: i + 1 for i, s in enumerate(strains)}
-
-metadata_file = "../../results/ST131_ABC/metadata.csv"
-mdf = pd.read_csv(metadata_file, index_col=0)
-
-alleles_file = "../../results/ST131_ABC/assembly_qc/alleles_summary.csv"
-adf = pd.read_csv(alleles_file, index_col=0)
-
-plasmid_mlst_file = "../../results/ST131_ABC/plasmids/alleles_summary.csv"
-pldf = pd.read_csv(plasmid_mlst_file, index_col=0)
 
 
-def parse_mlst_df(mlst_df_file):
-    df = pd.read_csv(mlst_df_file, sep=",", index_col=0, dtype=str)
-    df.fillna("-", inplace=True)
-    df["typ"] = "F" + df["FII"] + ":A" + df["FIA"] + ":B" + df["FIB"]
+def parse_input_data():
 
-    # remove untyped
-    mask = df["typ"] == "F-:A-:B-"
-    df = df[~mask]
-    return df
+    # tree
+    tree_file = "../../results/ST131_ABC/pangraph/asm20-100-5-filtered-coretree.nwk"
+    tree = Phylo.read(tree_file, "newick")
+    strains = [x.name for x in tree.get_terminals()]
+    strain_y = {s: i + 1 for i, s in enumerate(strains)}
+
+    # metadata
+    metadata_file = "../../results/ST131_ABC/metadata.csv"
+    mdf = pd.read_csv(metadata_file, index_col=0)
+
+    # MLST chromosome
+    alleles_file = "../../results/ST131_ABC/assembly_qc/alleles_summary.csv"
+    adf = pd.read_csv(alleles_file, index_col=0)
+
+    # plasmid MLST
+    plasmid_mlst_file = "../../results/ST131_ABC/plasmids/alleles_summary.csv"
+    pldf = pd.read_csv(plasmid_mlst_file, index_col=0)
+
+    def parse_mlst_df(mlst_df_file):
+        df = pd.read_csv(mlst_df_file, sep=",", index_col=0, dtype=str)
+        df.fillna("-", inplace=True)
+        df["typ"] = "F" + df["FII"] + ":A" + df["FIA"] + ":B" + df["FIB"]
+
+        # remove untyped
+        mask = df["typ"] == "F-:A-:B-"
+        df = df[~mask]
+        return df
+
+    pldf = parse_mlst_df(plasmid_mlst_file)
+
+    plasmid_json_file = "../../config/datasets/ST131_ABC/plasmids.json"
+    with open(plasmid_json_file, "r") as f:
+        plasmid_data = json.load(f)
+
+    # resistance
+    resistance_file = (
+        "../../results/ST131_ABC/plasmids/resistance/ncbi_summary_chromosome.csv"
+    )
+    rdf_pld = pd.read_csv(resistance_file, index_col=0)
+    resistance_file = "../../results/ST131_ABC/resistance/ncbi_summary.txt"
+    rdf_chr = pd.read_csv(resistance_file, index_col=0, sep="\t")
+    rdf_chr.index = rdf_chr.index.str.split("/").str[-1].str.removesuffix(".tab")
+    rdf_chr = rdf_chr.drop(columns=["NUM_FOUND"])
+
+    def convert(x):
+        if x == ".":
+            return 0
+        elif isinstance(x, str):
+            x = np.array([float(i) for i in x.split(";")])
+            x = np.sum(x > 80)
+            return x
+        else:
+            return int(x > 80)
+
+    rdf_chr = rdf_chr.map(convert)
+
+    return tree, strains, strain_y, mdf, adf, pldf, plasmid_data, rdf_chr, rdf_pld
 
 
-pldf = parse_mlst_df(plasmid_mlst_file)
-
-plasmid_json_file = "../../config/datasets/ST131_ABC/plasmids.json"
-with open(plasmid_json_file, "r") as f:
-    plasmid_data = json.load(f)
-
-resistance_file = (
-    "../../results/ST131_ABC/plasmids/resistance/ncbi_summary_chromosome.csv"
+tree, strains, strain_y, mdf, adf, pldf, plasmid_data, rdf_chr, rdf_pld = (
+    parse_input_data()
 )
-rdf_chr = pd.read_csv(resistance_file, index_col=0)
-resistance_file = "../../results/ST131_ABC/plasmids/resistance/ncbi_summary_plasmid.txt"
-rdf_pld = pd.read_csv(resistance_file, index_col=0, sep="\t")
+
+rdf_chr
+
 
 # %%
-fig, axs = plt.subplots(
-    1, 2, figsize=(8, 10), sharey=True, gridspec_kw={"width_ratios": [2, 1]}
-)
+def draw_legend(colors, svname, title=None):
+    figl, axl = plt.subplots(figsize=(3, 3))
+    for cont, col in colors.items():
+        axl.plot([], [], "s", color=col, label=cont)
+    axl.legend(title=title, loc="center")
+    axl.set_axis_off()
+    figl.tight_layout()
+    figl.savefig(str(svname) + ".pdf", bbox_inches="tight")
+    figl.savefig(str(svname) + ".svg", bbox_inches="tight")
+    plt.close(figl)
 
-ax = axs[0]
-Phylo.draw(tree, axes=ax, do_show=False, label_func=lambda x: "")
-right = ax.get_xlim()[1]
-ax.set_xlabel("")
-ax.set_ylabel("")
 
-for i, strain in enumerate(strains):
-    d = tree.distance(strain)
-    ax.plot([d, right], [i + 1, i + 1], ":", color="gray", lw=0.5)
+def draw_continent(ax, x, mdf, strains):
+    colors = {
+        cont: col
+        for cont, col in zip(
+            mdf["continent"].dropna().unique(), sns.color_palette("Set2")
+        )
+    }
+    for s in strains:
+        cont = mdf.loc[s, "continent"]
+        if cont is np.nan:
+            continue
+        y = strain_y[s]
+        col = colors[cont]
+        ax.plot(
+            x,
+            y,
+            "s",
+            color=col,
+            # markeredgecolor="black",
+            markersize=4,
+            markeredgewidth=0.5,
+        )
+    # legend
+    draw_legend(colors, fig_fld / "leg_continent", title="Continent")
 
-ax.set_xlim(right=right * 0.9)
 
-ax = axs[1]
-xticks = []
-xlabels = []
-# continent
-x = 0
-colors = {
-    cont: col
-    for cont, col in zip(mdf["continent"].dropna().unique(), sns.color_palette("Set2"))
-}
-for s in strains:
-    cont = mdf.loc[s, "continent"]
-    if cont is np.nan:
-        continue
-    y = strain_y[s]
-    col = colors[cont]
-    ax.plot(
-        x, y, "s", color=col, markeredgecolor="black", markersize=4, markeredgewidth=0.5
+def draw_year(ax, x, mdf, strains):
+    year_cat = pd.cut(
+        mdf["year"],
+        [2000, 2010, 2020, 2030],
+        labels=["2000-2010", "2010-2020", ">2020"],
     )
-xticks.append(x)
-xlabels.append("Continent")
-# legend
-figl, axl = plt.subplots()
-for cont, col in colors.items():
-    axl.plot([], [], "s", color=col, label=cont)
-axl.legend(title="Continent", loc="center")
-axl.set_axis_off()
+    colors = {
+        "2000-2010": sns.color_palette("Blues")[0],
+        "2010-2020": sns.color_palette("Blues")[2],
+        ">2020": sns.color_palette("Blues")[5],
+    }
+    for s in strains:
+        year = year_cat.loc[s]
+        if year is np.nan:
+            continue
+        y = strain_y[s]
+        col = colors[year]
+        ax.plot(
+            x,
+            y,
+            "s",
+            color=col,
+            markeredgecolor="none",
+            markersize=4,
+            markeredgewidth=0.5,
+        )
+    # legend
+    draw_legend(colors, fig_fld / "leg_year", title="Year")
 
 
-# years
-x = 1
-year_cat = pd.cut(
-    mdf["year"],
-    [2000, 2010, 2020, 2030],
-    labels=["2000-2010", "2010-2020", ">2020"],
-)
-colors = {
-    "2000-2010": sns.color_palette("Blues")[0],
-    "2010-2020": sns.color_palette("Blues")[2],
-    ">2020": sns.color_palette("Blues")[5],
-}
-for s in strains:
-    year = year_cat.loc[s]
-    if year is np.nan:
-        continue
-    y = strain_y[s]
-    col = colors[year]
-    ax.plot(
-        x, y, "s", color=col, markeredgecolor="black", markersize=4, markeredgewidth=0.5
-    )
-xticks.append(x)
-xlabels.append("Year")
-# legend
-figl, axl = plt.subplots()
-for year, col in colors.items():
-    axl.plot([], [], "s", color=col, label=year)
-axl.legend(title="Year", loc="center")
-axl.set_axis_off()
-
-ax.set_xticks(xticks)
-ax.set_xticklabels(xlabels, rotation=90)
-
-# alleles
-x = 3
-for al in ["fimH_eb", "gyrA_eb", "parC_eb"]:
-
+def draw_allele(ax, x, adf, strains):
     values = adf[al].dropna().value_counts().index[:3]
     colors = sns.color_palette("tab10", n_colors=len(values))
     colors = {v: c for v, c in zip(values, colors)}
@@ -145,17 +164,119 @@ for al in ["fimH_eb", "gyrA_eb", "parC_eb"]:
         c = colors[a] if a in colors else colors["other"]
         y = strain_y[s]
         ax.plot(x, y, "s", color=c, markersize=4, markeredgewidth=0.5)
-    xticks.append(x)
-    xlabels.append(al)
-    figl, axl = plt.subplots()
-    for value, col in colors.items():
-        try:
-            v = int(float(value))
-        except:
-            v = value
-        axl.plot([], [], "s", color=col, label=v)
-    axl.legend(title=al, loc="center")
 
+    colors = {str(v).split(".")[0]: c for v, c in colors.items()}
+
+    draw_legend(colors, fig_fld / f"leg_all_{al}", title=al.removesuffix("_eb"))
+
+
+def draw_chr_resistance(ax, x, strains, rdf, gene, color):
+    rs = rdf[gene]
+    for s in strains:
+        r = rs.loc[s]
+        if (r is np.nan) or (r < 1):
+            continue
+        y = strain_y[s]
+        ax.plot(
+            x,
+            y,
+            "o",
+            markerfacecolor="none",
+            markeredgecolor=color,
+            markersize=4,
+            markeredgewidth=0.5,
+        )
+
+
+def draw_pld_resistance(ax, x, strains, rdf, gene, color):
+    rs = rdf[gene]
+    for s in strains:
+        if not s in plasmid_data:
+            continue
+        r = rs.loc[s]
+        if (r is np.nan) or (r < 1):
+            continue
+        y = strain_y[s]
+        ax.plot(
+            x,
+            y,
+            "x",
+            markerfacecolor="none",
+            markeredgecolor=color,
+            markersize=4,
+            markeredgewidth=0.5,
+        )
+
+
+def draw_resistance_leg(svname, c_chrom, c_plasmid):
+    figl, axl = plt.subplots(figsize=(3, 3))
+    axl.plot(
+        [],
+        [],
+        "o",
+        markerfacecolor="none",
+        markeredgecolor=c_chrom,
+        markersize=4,
+        markeredgewidth=0.5,
+        label="on chromosome",
+    )
+    axl.plot(
+        [],
+        [],
+        "x",
+        markerfacecolor="none",
+        markeredgecolor=c_plasmid,
+        markersize=4,
+        markeredgewidth=0.5,
+        label="on plasmid",
+    )
+    axl.legend(title="Resistance", loc="center")
+    axl.set_axis_off()
+    figl.tight_layout()
+    figl.savefig(str(svname) + ".pdf", bbox_inches="tight")
+    figl.savefig(str(svname) + ".svg", bbox_inches="tight")
+    plt.close(figl)
+
+
+# %%
+fig, axs = plt.subplots(
+    1, 2, figsize=(6, 10), sharey=True, gridspec_kw={"width_ratios": [1, 1.2]}
+)
+
+# draw tree
+ax = axs[0]
+Phylo.draw(tree, axes=ax, do_show=False, label_func=lambda x: "")
+right = ax.get_xlim()[1]
+ax.set_xlabel("")
+ax.set_ylabel("")
+
+for i, strain in enumerate(strains):
+    d = tree.distance(strain)
+    ax.plot([d, right], [i + 1, i + 1], ":", color="gray", lw=0.5)
+
+ax.set_xlim(right=right * 0.9)
+
+
+ax = axs[1]
+xticks, xlabels = [], []
+# continent
+x = 0
+draw_continent(ax, x, mdf, strains)
+xticks.append(x)
+xlabels.append("Continent")
+
+# years
+x = 1
+draw_year(ax, x, mdf, strains)
+xticks.append(x)
+xlabels.append("Year")
+
+# alleles
+x = 3
+for al in ["fimH_eb", "gyrA_eb", "parC_eb"]:
+    draw_allele(ax, x, adf, strains)
+    xticks.append(x)
+    xlabels.append(al.removesuffix("_eb"))
     x += 1
 
 
@@ -183,8 +304,19 @@ for v in values:
     x += 1
 
 # resistance
-x = 13
-values = ["blaCTX-M-14", "blaCTX-M-15", "blaCTX-M-27"]
+x = 14
+c_chr = "#2a9d8f"
+c_plm = "#f4a261"
+values = ["blaCTX-M-14", "blaCTX-M-15", "blaCTX-M-27", "blaOXA-1", "blaTEM-1"]
+for v in values:
+    if v in rdf_chr.columns:
+        draw_chr_resistance(ax, x, strains, rdf_chr, v, c_chr)
+    if v in rdf_pld.columns:
+        draw_pld_resistance(ax, x, strains, rdf_pld, v, c_plm)
+    xticks.append(x)
+    xlabels.append(v)
+    x += 1
+draw_resistance_leg(svname=fig_fld / "leg_res", c_chrom=c_chr, c_plasmid=c_plm)
 
 
 # final setup
@@ -204,6 +336,8 @@ ax.yaxis.set_visible(False)
 
 
 plt.tight_layout()
+fig.savefig(fig_fld / "tree.pdf", bbox_inches="tight")
+fig.savefig(fig_fld / "tree.svg", bbox_inches="tight")
 plt.show()
 
 # %%
