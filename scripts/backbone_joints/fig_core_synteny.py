107eb0d4995b0022c60bd0a28eb078ddd34aa12d
Marco Molari <mmolari91@gmail.com>
2024-03-25 10:16:25 +0100

    fix: core synteny figure

--- scripts/backbone_joints/fig_core_synteny.py
+++ scripts/backbone_joints/fig_core_synteny.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import seaborn as sns
 import pypangraph as pp
 from Bio import Phylo
@@ -99,7 +100,7 @@ def fig_syntey(path_cats, common_path, strand_common, bdf, svname):
     fig, ax = plt.subplots(figsize=(10, 10))
 
     # cmap = plt.cm.get_cmap("tab20b")(range(len(bdf)))
-    cmap = plt.colormaps.get_cmap("nipy_spectral")(np.linspace(0, 1, len(bdf)))
+    cmap = mpl.cm.get_cmap("nipy_spectral")(np.linspace(0, 1, len(bdf)))
     block_colors = defaultdict(lambda: cmap[len(block_colors)])
 
     cmap_iso = armytage_cmap()
@@ -229,7 +230,7 @@ if __name__ == "__main__":
 
     # make folder and figure names
     fig_fld = pathlib.Path(args.fig_fld)
-    fig_fld.mkdir(exist_ok=True)
+    fig_fld.mkdir(exist_ok=True, parents=True)
 
     svfig_synt = fig_fld / f"core_synteny"
     svfig_tree = fig_fld / f"tree"
